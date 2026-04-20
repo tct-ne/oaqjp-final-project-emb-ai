83c5dee75da90279c786aaaf692d04610ae92804
tct-ne <terri.cassells@technology1.com>
2026-04-19 23:02:25 -0400

    Task 7, 8

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Flask server for Emotion Detection application.
+Provides an API endpoint to analyze text emotions using Watson NLP.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,8 +9,15 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the emotion scores 
+        and the dominant emotion for the the provided text.
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
+    if result["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
     anger = result["anger"]
     disgust = result["disgust"]
     fear = result["fear"]
@@ -22,6 +33,9 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
